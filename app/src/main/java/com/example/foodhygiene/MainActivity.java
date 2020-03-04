57f8dcd5b9d5440a604cc9c86be6c9a3d9b2ff95
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2020-03-04 18:48:19 +0000

    added gps location search

--- app/src/main/java/com/example/foodhygiene/MainActivity.java
+++ app/src/main/java/com/example/foodhygiene/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.foodhygiene;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -28,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
     EditText input;
     TextView bName;
     Button reset;
-   // ScrollView scroll;
+    Button locate;
+    double lat;
+    double lng;
+    TextView longitude;
+    TextView latitude;
+
 
     //String businessInfo;
     @Override
@@ -43,7 +54,58 @@ public class MainActivity extends AppCompatActivity {
         input = findViewById(R.id.input);
         bName = findViewById(R.id.bName);
         reset = findViewById(R.id.reset);
-       // scroll = findViewById(R.id.scroll);
+        locate = findViewById(R.id.reset);
+        longitude = findViewById(R.id.longitude);
+        latitude = findViewById(R.id.latitude);
+        String[] requiredPermissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+
+        };
+        boolean ok = true;
+        for (int i = 0 ; i < requiredPermissions.length; i++) {
+            int result = ActivityCompat.checkSelfPermission(this, requiredPermissions[i]);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                ok = false;
+            }
+        }
+        if (!ok){
+            ActivityCompat.requestPermissions(this, requiredPermissions, 1);
+            System.exit(0);
+        }
+
+        else {
+            LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+                    latitude.setText(""+lat);
+                    longitude.setText(""+ lng);
+
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+
+        }
+
+
 
 
     }
@@ -51,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
     public void on_click (View v)
     {
         ArrayList<String> Name = new ArrayList<String>();
-       // String names = getResources().getString(Name.get(0));
+        // String names = getResources().getString(Name.get(0));
         ArrayList<String> Rate = new ArrayList<String>();
 
         // try catch
@@ -68,25 +130,25 @@ public class MainActivity extends AppCompatActivity {
             while ((line = in.readLine()) != null) {
                 rb = rb + line;
             }
-                JSONArray ja = new JSONArray(rb);
+            JSONArray ja = new JSONArray(rb);
 
-                for (int i = 0; i < ja.length(); i ++)
-                {
-                    JSONObject jo = (JSONObject) ja.get(i);
-                    Name.add(jo.getString("BusinessName"));
-                   // bName.setText(Name.get(0));
+            for (int i = 0; i < ja.length(); i ++)
+            {
+                JSONObject jo = (JSONObject) ja.get(i);
+                Name.add(jo.getString("BusinessName"));
+                // bName.setText(Name.get(0));
 
-                   // bName.append(Name.get(0));
-                    businessInfo += "Business name : " + jo.get("BusinessName") + "\n"
-                            + "Business Address : " +jo.get("AddressLine2" ) + "\n"
-                            + "Postcode : " +jo.get("PostCode" ) + "\n"
-                            + "Rating : "+ jo.get("RatingValue" ) + "\n"
-                            + "\n ______________________ \n";
+                // bName.append(Name.get(0));
+                businessInfo += "Business name : " + jo.get("BusinessName") + "\n"
+                        + "Business Address : " +jo.get("AddressLine2" ) + "\n"
+                        + "Postcode : " +jo.get("PostCode" ) + "\n"
+                        + "Rating : "+ jo.get("RatingValue" ) + "\n"
+                        + "\n ______________________ \n";
 
-                }
-                bName.setText(businessInfo);
+            }
+            bName.setText(businessInfo);
 
-               // in.close();
+            // in.close();
                /* for (int i = 0; i < ja.length(); i ++)
                 {
                     JSONObject jo = (JSONObject) ja.get(i);
@@ -107,9 +169,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public void on_click_locate (View v)
+    {
+        ArrayList<String> Name = new ArrayList<String>();
+        System.out.println(lat);
+        System.out.println(lng);
+
+        // try catch
+
+        try {
+
+            URL url = new URL("http://sandbox.kriswelsh.com/hygieneapi/hygiene.php?op=search_location&lat=" + lat+ "&long=" + lng);
+            URLConnection tc = url.openConnection();
+            InputStreamReader isr = new InputStreamReader(tc.getInputStream());
+            BufferedReader in = new BufferedReader(isr);
+
+            String line = "" , rb= "" ,businessInfo = "";
+
+            while ((line = in.readLine()) != null) {
+                rb = rb + line;
+            }
+            JSONArray ja = new JSONArray(rb);
+
+            for (int i = 0; i < ja.length(); i ++)
+            {
+                JSONObject jo = (JSONObject) ja.get(i);
+                Name.add(jo.getString("BusinessName"));
+                // bName.setText(Name.get(0));
+
+                // bName.append(Name.get(0));
+                businessInfo += "Business name : " + jo.get("BusinessName") + "\n"
+                        + "Business Address : " +jo.get("AddressLine2" ) + "\n"
+                        + "Postcode : " +jo.get("PostCode" ) + "\n"
+                        + "Rating : "+ jo.get("RatingValue" ) + "\n"
+                        + "\n ______________________ \n";
+
+            }
+            bName.setText(businessInfo);
+
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public  void reset (View v){
         bName.setText("");
         input.setText("");
+        latitude.setText("");
+        longitude.setText("");
 
     }
 
